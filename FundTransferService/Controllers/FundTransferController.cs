524a49b3e66f0d3b6a973125c31c5239e8f59e9f
Aslam <aslam.n@appliedsni.com>
2022-04-05 14:52:22 +0530

    Initial Code

---
+++ FundTransferService/Controllers/FundTransferController.cs
@@ -0,0 +1,53 @@
+﻿using FundTransferService.Database;
+using FundTransferService.Database.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace FundTransferService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FundTransferController : ControllerBase
+    {
+        DatabaseContext db;
+
+        public FundTransferController(DatabaseContext datacontext)
+        {
+            db = datacontext;
+        }
+
+        [HttpGet("GetCustomers")]
+        public IEnumerable<Customers> GetCustomers()
+        {
+            return db.Customers.ToList();
+        }
+
+        [HttpGet("GetCustomerById/{id}")]
+        public Customers GetCustomerById(int id)
+        {
+            return db.Customers.Find(id);
+        }
+
+        [HttpPost("AddCustomer")]
+        public IActionResult AddCustomer([FromBody] Customers customerModel)
+        {
+            try
+            {
+                db.Customers.Add(customerModel);
+                db.SaveChanges();
+                return StatusCode(StatusCodes.Status201Created, customerModel);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex);
+                throw;
+            }
+        }
+
+    }
+}
---
+++ FundTransferService/Database/DatabaseContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FundTransferService.Database.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace FundTransferService.Database
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Customers> Customers { get; set; }
+    }
+}
